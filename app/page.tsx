8c3bcafe3d5627806eb2a32d9b096ac3d0937317
Maksuda123 <afraazra171@gmail.com>
2024-05-13 15:05:38 +0600

    For righside image add Pentagon background shape

--- app/page.tsx
+++ app/page.tsx
@@ -1,153 +1,111 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
 
+export default function Signup() {
+  const [showPassword, setShowPassword] = useState(false);
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
-// export default function Example() {
-//   return (
-//     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-//       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-//         <img
-//           className="mx-auto h-12 w-auto"
-//           src="https://logodix.com/logo/1811215.png?color=indigo&shade=600"
-//           alt="Ati Limited"
-//         />
-//         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-//       </div>
-
-//       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-//         <form className="space-y-6" action="#" method="POST">
-//           <div>
-//             <label htmlFor="user" className="block text-sm font-medium text-gray-700">
-//               User Name:
-//             </label>
-//             <div className="mt-1">
-//               <input
-//                 id="user"
-//                 name="user"
-//                 type="text"
-//                 autoComplete="username"
-//                 required
-//                 className="appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//               />
-//             </div>
-//           </div>
-
-//           <div>
-//             <div className="flex justify-between">
-//               <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-//                 Password:
-//               </label>
-//               <div className="text-sm">
-//                 <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-//                   Forgot password?
-//                 </a>
-//               </div>
-//             </div>
-//             <div className="mt-1">
-//               <input
-//                 id="password"
-//                 name="password"
-//                 type="password"
-//                 autoComplete="current-password"
-//                 required
-//                 className="appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-//               />
-//             </div>
-//           </div>
-
-//           <div>
-//             <button
-//               type="submit"
-//               className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-semibold text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-//             >
-//               Sign in
-//             </button>
-//           </div>
-//         </form>
-
-//         <p className="mt-2 text-center text-sm text-gray-600">
-//           Not signed up?{' '}
-//           <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-//             Sign up
-//           </a>
-//         </p>
-//       </div>
-//     </div>
-//   )
-// }
-
-
-
-
-export default function Example() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-indigo-500 via-purple-600 to-pink-500 flex items-center justify-center">
-      <div className="bg-white bg-opacity-75 rounded-lg shadow-lg p-8 max-w-md w-full">
-        <div className="text-center">
-          <img
-            className="mx-auto h-12 w-auto"
-            src="https://logodix.com/logo/1811215.png?color=indigo&shade=600"
-            alt="Ati Limited"
-          />
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
-        </div>
+    <div className="min-h-screen bg-gradient-to-b from-orange-200 via-orange-300 to-green-400 flex items-center justify-center">
+      <div className="bg-zinc-300 bg-opacity-75 rounded-lg shadow-lg p-4 max-w-md w-full space-y-8 flex relative">
+        {/* Pentagon shape */}
+        <div className="fixed m-28 flex items-center pointer-events-none z-10">
+          <svg
+            className="text-orange-300"
+            width="350"
+            height="380"
+            viewBox="0 0 350 380"
+            fill="currentColor"
+          >
+            <polygon points="200 50, 300 140, 280 300, 120 300, 100 140" />
+          </svg>
+        </div> 
 
-        <form className="mt-8 space-y-6" action="#" method="POST">
-          <div>
-            <label htmlFor="user" className="block text-sm font-medium text-gray-700">
-              User Name:
-            </label>
-            <div className="mt-1">
+        {/* Signup content */}
+        <div className="w-1/2 z-10 mr-2">
+          <div className="text-center">
+            <div className="text-center"></div>
+            <Image src="/logo.svg" alt="Logo" width={50} height={15} />
+            <h2 className="mt-6 text-2xl font-extrabold text-gray-900">
+              Sign in to get started!
+            </h2>
+          </div>
+
+          <form className="mt-2 space-y-6" action="#" method="POST">
+            <div>
               <input
                 id="user"
                 name="user"
                 type="text"
+                placeholder="User Name"
                 autoComplete="username"
                 required
-                className="appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-100"
+                className="block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-100"
               />
             </div>
-          </div>
-
-          <div>
-            <div className="flex justify-between">
-              <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                Password:
-              </label>
-              <div className="text-sm">
-                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                  Forgot password?
-                </a>
-              </div>
-            </div>
-            <div className="mt-1">
+            <div className="relative">
               <input
                 id="password"
                 name="password"
-                type="password"
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
                 autoComplete="current-password"
                 required
                 className="appearance-none block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-gray-100"
               />
+              <button
+                type="button"
+                className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                onClick={togglePasswordVisibility}
+              >
+                <Image
+                  src="/eye-password-see-view 1.svg"
+                  alt="Eye Icon"
+                  width={15}
+                  height={15}
+                />
+              </button>
             </div>
-          </div>
-
-          <div>
+            <div className="text-right">
+              <a
+                href="#"
+                className="text-sm font-semibold text-amber-600 hover:text-amber-400"
+              >
+                Forgot password?
+              </a>
+            </div>
             <button
               type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-semibold text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-semibold text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Sign in
             </button>
-          </div>
-        </form>
+          </form>
 
-        <p className="mt-2 text-center text-sm text-gray-600">
-          Not signed up?{' '}
-          <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-            Sign up
-          </a>
-        </p>
+          <p className="mt-2 text-sm text-gray-600">
+            Not signed up?{" "}
+            <a
+              href="#"
+              className="font-semibold text-amber-600 hover:text-amber-500"
+            >
+              Sign up
+            </a>
+          </p>
+        </div>
+
+        {/* Image */}
+        <div className="w-1/2 flex justify-center items-center z-10">
+          <img
+            src="/Women pic.png"
+            alt="Doctor Image"
+            className="w-30 h-30 ml-4 object-cover rounded-lg shadow-lg"
+          />
+        </div>
       </div>
     </div>
-  )
+  );
 }
